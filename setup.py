0c8ce233fcf78e94eb475fe3676964c312d18810
Allen Gooch <allen.gooch@gmail.com>
2018-05-26 00:33:19 -0500

    Add long description to package metadata

--- setup.py
+++ setup.py
@@ -6,11 +6,16 @@ from configaro.__metadata__ import release_metadata as configuration
 
 packages = find_packages()
 
+with open('README.md') as infile:
+    long_description = infile.read()
+
 with open('requirements.txt') as infile:
     requirements = [line.strip() for line in infile.readlines()]
 
 configuration.update(dict(
     packages=packages,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=requirements,
     zip_safe=True,
     test_suite='tests',
